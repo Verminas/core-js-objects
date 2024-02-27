6901a4492a4e8b5167d1f91cd416a3aeb822feca
Verminas <veremeeva96@gmail.com>
2024-02-27 19:25:32 +0300

    feat: add solution for sortCitiesArray

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -278,8 +278,22 @@ function fromJSON(proto, json) {
  *      { country: 'Russia',  city: 'Saint Petersburg' }
  *    ]
  */
-function sortCitiesArray(/* arr */) {
-  throw new Error('Not implemented');
+function sortCitiesArray(arr) {
+  return arr.sort((a, b) => {
+    let result;
+    if (a.country < b.country) {
+      result = -1;
+    } else if (a.country > b.country) {
+      result = 1;
+    } else if (a.country === b.country) {
+      if (a.city < b.city) {
+        result = -1;
+      } else {
+        result = 1;
+      }
+    }
+    return result;
+  });
 }
 
 /**
