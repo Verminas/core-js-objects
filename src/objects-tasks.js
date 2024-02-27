a21128f0590de396f87dc8c4b93ebe306c56da09
Verminas <veremeeva96@gmail.com>
2024-02-27 19:12:59 +0300

    feat: add solution for fromJSON

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -246,8 +246,10 @@ function getJSON(obj) {
  *    const r = fromJSON(Circle.prototype, '{"radius":10}');
  *
  */
-function fromJSON(/* proto, json */) {
-  throw new Error('Not implemented');
+function fromJSON(proto, json) {
+  const data = JSON.parse(json);
+  const object = Object.create(proto);
+  return Object.assign(object, data);
 }
 
 /**
